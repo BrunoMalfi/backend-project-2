f791dd5a2b1f99a685a3a30ce4c33eda4472ff02
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-05-10 17:17:52 +0200

    "change UserController"

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,10 +20,5 @@ const UserController = {
             console.error(error);
         }
     },
-<<<<<<< HEAD
 };
 module.exports = UserController;
-=======
-}; 
-module.exports = UserController;
->>>>>>> bruno
