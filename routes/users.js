a7d9733a391fa2b9fa7abd52d1cb19afce4c3834
[DaniellaBarraza125] <daniellabarraza125@gmail.com>
2024-05-10 13:11:51 +0200

    testing git

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router()
-const UserController = require('../controllers/UserController');
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/UserController");
 
-router.post('/new',UserController.create)
+router.post("/new", UserController.create);
+router.get("/", UserController.getAll);
 
 module.exports = router;
