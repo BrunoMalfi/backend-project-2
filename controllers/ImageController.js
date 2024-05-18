c40eead1a2a58eae9fdce3e2908bfd062176fe7f
BrunoMalfi <malfi.bruno@gmail.com>
2024-05-18 17:13:09 +0200

    updates to debug in server

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -6,10 +6,9 @@ const ImageController= {
         const imageName = req.params.imageName;
         const imagePath = path.join(staticDir, imageName);
         const mainDirPath = path.join(__dirname, '..');
-        console.log('ciao ', mainDirPath)
         res.sendFile(mainDirPath+"/"+imagePath, (error) => {
           if (error) {
-            res.status(404).send('Image not found');
+            res.status(404).send('Image not found at : ' +mainDirPath + " / "+ imagePath);
           }
         });
     }
